e7e75c94acf4d4af1509c6b12217edd093daf806
ibrahim kanber <ibrahim.kanber@qconnex.com>
2022-07-23 23:41:09 +0200

    feature/form-validation create tab switch component

---
+++ src/components/tab_switch/TabSwitch.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Routes, useMainContext} from '../../providers/MainProvider';
+import {theme} from '../../theming';
+import {Button} from '../ui/Button';
+
+const Buttons = [
+  {title: Routes.HOME, id: 1, route: Routes.HOME},
+  {title: Routes.RECORDS, id: 2, route: Routes.RECORDS},
+  {title: Routes.SETTINGS, id: 3, route: Routes.SETTINGS},
+];
+export const TabSwitch = () => {
+  const {setSelectedTab} = useMainContext();
+  const handleSwitch = (route: Routes) => {
+    setSelectedTab?.(route);
+  };
+
+  return (
+    <View style={styles.container}>
+      {Buttons.map(button => (
+        <Button
+          key={button.id}
+          title={button.title}
+          onPress={() => handleSwitch(button.route)}
+          containerStyle={styles.button}
+        />
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    padding: theme.spaces.xs,
+    paddingTop: theme.spaces.xxl,
+  },
+  title: {
+    color: theme.colorOnPrimary,
+  },
+  button: {
+    marginHorizontal: theme.spaces.xs,
+    paddingHorizontal: theme.spaces.l,
+  },
+});
