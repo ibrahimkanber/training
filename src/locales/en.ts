038786e8eec56d806f52b300f08c951e8772556d
ibrahim kanber <ibrahim.kanber@qconnex.com>
2022-07-24 11:23:23 +0200

    feature/form-validation final touch

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -2,13 +2,13 @@ import {Fields} from '../components/record/constants';
 
 export const translations_en = {
   error: {
-    [Fields.FIRST_NAME]: 'First name error--lang=en',
-    [Fields.STREET_NAME]: 'Street name--error--lang=en',
-    [Fields.STREET_NUMBER]: 'Street number--error--lang=en',
-    [Fields.COMPANY_NAME]: 'Company name--error--lang=en',
-    [Fields.TOWN]: 'Town--error--lang=en',
-    [Fields.ZIP_CODE]: 'Zip Code--error--lang=en',
-    [Fields.PHONE_NUMBER]: 'Phone Number--error--lang=en',
+    [Fields.FIRST_NAME]: 'First name error',
+    [Fields.STREET_NAME]: 'Street name error',
+    [Fields.STREET_NUMBER]: 'Street number error',
+    [Fields.COMPANY_NAME]: 'Company name error',
+    [Fields.TOWN]: 'Town-error',
+    [Fields.ZIP_CODE]: 'Zip Code error',
+    [Fields.PHONE_NUMBER]: 'Phone Number error',
   },
   fieldNames: {
     [Fields.FIRST_NAME]: 'First Name',
