eb13b3390003f9ad1f4474efcece9ee58616fcb1
ibrahim kanber <ibrahim.kanber@qconnex.com>
2022-07-24 09:56:51 +0200

    feature/form-validation bug fix translations

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -2,13 +2,13 @@ import {Fields} from '../components/record/constants';
 
 export const translations_en = {
   error: {
-    [Fields.FIRST_NAME]: 'firstName error--lang=en',
-    [Fields.STREET_NAME]: 'streetName--error--lang=en',
-    [Fields.STREET_NUMBER]: 'streetName--error--lang=en',
-    [Fields.COMPANY_NAME]: 'streetName--error--lang=en',
-    [Fields.TOWN]: 'streetName--error--lang=en',
-    [Fields.ZIP_CODE]: 'streetName--error--lang=en',
-    [Fields.PHONE_NUMBER]: 'streetName--error--lang=en',
+    [Fields.FIRST_NAME]: 'First name error--lang=en',
+    [Fields.STREET_NAME]: 'Street name--error--lang=en',
+    [Fields.STREET_NUMBER]: 'Street number--error--lang=en',
+    [Fields.COMPANY_NAME]: 'Company name--error--lang=en',
+    [Fields.TOWN]: 'Town--error--lang=en',
+    [Fields.ZIP_CODE]: 'Zip Code--error--lang=en',
+    [Fields.PHONE_NUMBER]: 'Phone Number--error--lang=en',
   },
   fieldNames: {
     [Fields.FIRST_NAME]: 'First Name',
